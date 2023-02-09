0693b1539ce7c646826d34a3df2aa50582f27333
yourseulah <imseulahlee@gmail.com>
2023-02-09 02:19:38 -0800

    icon
    icon/logo implement

--- app/src/main/java/com/example/petgrooming/MainActivity.java
+++ app/src/main/java/com/example/petgrooming/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.petgrooming;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setLogo(R.mipmap.ic_launcher_paw_round);
+        actionBar.setTitle("PawPaw");
+
     }
 }
