34fdf742213eabe27af11ca87f18e1e8ded7b74c
Sri Vani Chaitanya Varma Datla <chaitanyavarmadatla1998@gmail.com>
2023-02-25 18:11:47 -0800

    Added basic functionality for downloading content as PDF file

--- app/src/main/java/com/example/petgrooming/CheckOutActivity.java
+++ app/src/main/java/com/example/petgrooming/CheckOutActivity.java
@@ -1,14 +1,151 @@
 package com.example.petgrooming;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CheckOutActivity extends AppCompatActivity {
 
+    Button btnDownloadPDF;
+
+    int pageHeight = 1120;
+    int pagewidth = 792;
+
+    Bitmap bmp, scaledbmp;
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
+
+
+
+
+
+        //custom - Sri: Start
+
+        btnDownloadPDF = findViewById(R.id.btnDownloadPdf);
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.androidstudio);
+        scaledbmp = Bitmap.createScaledBitmap(bmp, 140, 140, false);
+
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
+
+        btnDownloadPDF.setOnClickListener((View v) -> {
+                generatePDF();
+        });
     }
+
+    private void generatePDF() {
+
+        PdfDocument pdfDocument = new PdfDocument();
+
+        Paint paint = new Paint();
+        Paint title = new Paint();
+
+
+        PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth,
+                pageHeight, 1).create();
+
+        PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
+
+        Canvas canvas = myPage.getCanvas();
+
+        canvas.drawBitmap(scaledbmp, 56, 40, paint);
+
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+
+        title.setTextSize(15);
+
+        title.setColor(ContextCompat.getColor(this, R.color.black));
+
+        canvas.drawText("Excited to meet you soon!", 209, 100, title);
+        canvas.drawText("Paw Paw", 209, 80, title);
+
+        title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        title.setColor(ContextCompat.getColor(this, R.color.black));
+        title.setTextSize(15);
+
+        title.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText("Your Order Number: ", 396, 560, title);
+
+        pdfDocument.finishPage(myPage);
+
+        //File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),"ordersummary.pdf");
+        // getFilesDir() is working
+        File file = new File(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DCIM),
+                "OrderSummary.pdf");
+        // PDF Path is - /storage/emulated/0/Android/data/com.example.petgrooming/files/DCIM
+
+
+        try {
+
+            pdfDocument.writeTo(new FileOutputStream(file));
+            Log.i("pdf path",getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DCIM).toString());
+            Toast.makeText(CheckOutActivity.this, "PDF file generated successfully.",
+                    Toast.LENGTH_SHORT).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pdfDocument.close();
+    }
+
+    private boolean checkPermission() {
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).
+                            show();
+                } else {
+                    Toast.makeText(this, "Permission Denied.",
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
+    }
+    // custom:Sri - End
 }
+
+
