bb23900f8e5f1dc9f29b9e41ba4f2b056a024187
David Salazar <dssalaza@gmail.com>
2023-03-27 13:33:12 -0700

    Added firebase photo upload integration to drawer/profile image

--- app/src/main/java/com/example/petgrooming/HomeScreenMainActivity.java
+++ app/src/main/java/com/example/petgrooming/HomeScreenMainActivity.java
@@ -1,18 +1,21 @@
 package com.example.petgrooming;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,18 +24,32 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 
 public class HomeScreenMainActivity extends NavigationBar implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    ImageView navProfileImgView; //Image view inside the drawerLayout
     Toolbar toolbar;
 
     TextView userName;
+
+    //Firebase
+    Uri imageUri;
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     GoogleSignInClient gClient;
     GoogleSignInOptions gOptions;
@@ -66,8 +83,14 @@ public class HomeScreenMainActivity extends NavigationBar implements NavigationV
                 this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this); //onNavigationItemSelected
         navigationView.setCheckedItem(R.id.nav_home);
+
+        //Firebase instrumentation
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        View headerLayout = navigationView.getHeaderView(0);
+        navProfileImgView = headerLayout.findViewById(R.id.navProfileImgView);
 
         //login Information to be displayed--------------------------------
         userName = findViewById(R.id.txtViewUserName);
@@ -83,7 +106,6 @@ public class HomeScreenMainActivity extends NavigationBar implements NavigationV
             String gName = gAccount.getDisplayName();
             userName.setText(gName);
         }
-
 
         bottomNavigationView = findViewById(R.id.bottonnav);
         bottomNavigationView.setOnItemSelectedListener(this);
@@ -106,6 +128,14 @@ public class HomeScreenMainActivity extends NavigationBar implements NavigationV
             intent.putExtra("petAnimalTypeStringExtra", "Cat");
             startActivity(intent);
         });
+
+        navProfileImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseProfilePicture();
+            }
+        });
+
     }
 
     @Override
@@ -144,5 +174,59 @@ public class HomeScreenMainActivity extends NavigationBar implements NavigationV
         return R.id.bottonnav;
     }
 
+    private void chooseProfilePicture(){
+        try {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intent,1);
 
+        }catch (Exception e){
+            Toast.makeText(this, "Something went wrong while selecting the picture", Toast.LENGTH_SHORT).show();
+            Log.d("UPLOAD_PHOTO","Error:" + e);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode==RESULT_OK && data.getData()!=null){
+            imageUri = data.getData();
+            navProfileImgView.setImageURI(imageUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture(){
+
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading Image...");
+        pd.show();
+
+        final String uniqueID = UUID.randomUUID().toString();
+
+        // Create a reference to the image uniqueID created
+        StorageReference imageRef = storageReference.child("images/" + uniqueID);
+
+        imageRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getApplicationContext(),"Photo uploaded", Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Photo not uploaded", Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                pd.setMessage("Percentage: " + (int) progressPercent + "%");
+            }
+        });
+
+    }
 }
